563f6dc885db90c848914b7de6c5dde49fe4e41c
negitoro.2007 <negitoro.2007@gmail.com>
2025-02-05 10:48:44 +0900

    render対応

--- src/app.js
+++ src/app.js
@@ -1,32 +1,45 @@
-'use strict';
+"use strict";
 
-const { Hono } = require('hono');
-const { logger } = require('hono/logger');
-const { html } = require('hono/html');
-const { HTTPException } = require('hono/http-exception');
-const { secureHeaders } = require('hono/secure-headers');
-const { env } = require('hono/adapter');
-const { serveStatic } = require('@hono/node-server/serve-static');
-const { trimTrailingSlash } = require('hono/trailing-slash');
-const { githubAuth } = require('@hono/oauth-providers/github');
-const { getIronSession } = require('iron-session');
-const { PrismaClient } = require('@prisma/client');
-const layout = require('./layout');
+const { Hono } = require("hono");
+const { csrf } = require("hono/csrf");
+const { logger } = require("hono/logger");
+const { html } = require("hono/html");
+const { HTTPException } = require("hono/http-exception");
+const { secureHeaders } = require("hono/secure-headers");
+const { env } = require("hono/adapter");
+const { getCookie, deleteCookie } = require("hono/cookie");
+const { serveStatic } = require("@hono/node-server/serve-static");
+const { trimTrailingSlash } = require("hono/trailing-slash");
+const { githubAuth } = require("@hono/oauth-providers/github");
+const { getIronSession } = require("iron-session");
+const { PrismaClient } = require("@prisma/client");
+const layout = require("./layout");
 
-const prisma = new PrismaClient({ log: ['query'] });
+const prisma = new PrismaClient({ log: ["query"] });
 
-const indexRouter = require('./routes/index');
-const loginRouter = require('./routes/login');
-const logoutRouter = require('./routes/logout');
-const scheduleRouter = require('./routes/schedules');
-const availabilitiesRouter = require('./routes/availabilities');
-const commentsRouter = require('./routes/comments');
+const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+const scheduleRouter = require("./routes/schedules");
+const availabilitiesRouter = require("./routes/availabilities");
+const commentsRouter = require("./routes/comments");
 
 const app = new Hono();
 
+app.use(async (c, next) => {
+  const { CSRF_TRUSTED_ORIGIN } = env(c);
+  const handler = csrf({
+    origin: CSRF_TRUSTED_ORIGIN,
+  });
+  await handler(c, next);
+});
 app.use(logger());
-app.use(serveStatic({ root: './public' }));
-app.use(secureHeaders());
+app.use(serveStatic({ root: "./public" }));
+app.use(
+  secureHeaders({
+    referrerPolicy: "strict-origin-when-cross-origin",
+  })
+);
 app.use(trimTrailingSlash());
 
 // セッション管理用のミドルウェア
@@ -34,28 +47,28 @@ app.use(async (c, next) => {
   const { SESSION_PASSWORD } = env(c);
   const session = await getIronSession(c.req.raw, c.res, {
     password: SESSION_PASSWORD,
-    cookieName: 'session',
+    cookieName: "session",
   });
-  c.set('session', session);
+  c.set("session", session);
   await next();
 });
 
 // GitHub 認証
-app.use('/auth/github', async (c, next) => {
+app.use("/auth/github", async (c, next) => {
   const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = env(c);
   const authHandler = githubAuth({
     client_id: GITHUB_CLIENT_ID,
     client_secret: GITHUB_CLIENT_SECRET,
-    scope: ['user:email'],
+    scope: ["user:email"],
     oauthApp: true,
   });
   return await authHandler(c, next);
 });
 
 // GitHub 認証の後の処理
-app.get('/auth/github', async (c) => {
-  const session = c.get('session');
-  session.user = c.get('user-github');
+app.get("/auth/github", async (c) => {
+  const session = c.get("session");
+  session.user = c.get("user-github");
   await session.save();
 
   // ユーザ情報をデータベースに保存
@@ -70,29 +83,36 @@ app.get('/auth/github', async (c) => {
     create: data,
   });
 
-  return c.redirect('/');
+  const loginForm = getCookie(c, "loginForm");
+
+  if (loginForm && loginForm.startsWith("/")) {
+    deleteCookie(c, "loginForm");
+    return c.redirect(loginForm);
+  } else {
+    return c.redirect("/");
+  }
 });
 
 // ルーティング
-app.route('/', indexRouter);
-app.route('/login', loginRouter);
-app.route('/logout', logoutRouter);
-app.route('/schedules', scheduleRouter);
-app.route('/schedules', availabilitiesRouter);
-app.route('/schedules', commentsRouter);
+app.route("/", indexRouter);
+app.route("/login", loginRouter);
+app.route("/logout", logoutRouter);
+app.route("/schedules", scheduleRouter);
+app.route("/schedules", availabilitiesRouter);
+app.route("/schedules", commentsRouter);
 
 // 404 Not Found
 app.notFound((c) => {
   return c.html(
     layout(
       c,
-      'Not Found',
+      "Not Found",
       html`
         <h1>Not Found</h1>
         <p>${c.req.url} の内容が見つかりませんでした。</p>
-      `,
+      `
     ),
-    404,
+    404
   );
 });
 
@@ -104,15 +124,15 @@ app.onError((error, c) => {
   return c.html(
     layout(
       c,
-      'Error',
+      "Error",
       html`
         <h1>Error</h1>
         <h2>${error.name} (${statusCode})</h2>
         <p>${error.message}</p>
-        ${NODE_ENV === 'development' ? html`<pre>${error.stack}</pre>` : ''}
-      `,
+        ${NODE_ENV === "development" ? html`<pre>${error.stack}</pre>` : ""}
+      `
     ),
-    statusCode,
+    statusCode
   );
 });
 
